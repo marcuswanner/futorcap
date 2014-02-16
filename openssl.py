f319ac01d217204bb689ccf02ed7696807e88f4e
Marcus Wanner <marcus@wanners.net>
2014-02-16 18:15:27 -0500

    Buggy but should have all basic futord features

---
+++ openssl.py
@@ -0,0 +1,13 @@
+##
+# OpenSSL key generation module for futorcap.
+
+import subprocess
+
+def generate(pubfname=None, privfname=None, bits=2048):
+    assert pubfname is not None and privfname is not None, \
+        "Both pubfname and privfname are required!"
+    c = subprocess.call(["openssl", "genrsa", "-out", privfname, str(bits)])
+    assert c == 0, "Privkey generation failed"
+    c = subprocess.call(["openssl", "rsa", "-in", privfname,
+        "-out", pubfname, "-outform", "PEM"])
+    assert c == 0, "Pubkey generation failed"
